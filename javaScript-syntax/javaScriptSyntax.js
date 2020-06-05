952d7ef2db569b277c64139843b746614d5f9028
ibrahimalkhooly10 <ibrahimalkhooly10@gmail.com>
2020-06-05 08:49:17 +0300

    I wrote my first code

--- javaScript-syntax/javaScriptSyntax.js
+++ javaScript-syntax/javaScriptSyntax.js
@@ -1,3 +1,21 @@
 let ibrahim = 'ibrahim'
 let age = 10
 let grade = 'fifth grade '
+alert('hello there');
+alert('hello there')
+undefined
+confirm('how are you')
+false
+confirm('how are you');
+true
+prompt('are how are you ');
+""
+prompt('how are you')
+"very good"
+console('are you fine');
+console('are you good');
+console('what are you doing');
+//ibrahim is sleepy boy
+/*
+this is a multi line comment
+*/
